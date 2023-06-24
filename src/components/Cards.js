690a97fb498398c85362b0e7da79099e6081f563
roy2pushkar <roy2000pushkar@gmail.com>
2023-06-25 01:28:27 +0530

    Top courses

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,25 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "./Card";
-import { toast } from "react-toastify";
+import { useState } from "react";
 
 const Cards = (props) => {
   let courses = props.courses;
-  //console.log("Printing Data");
+  let category = props.category;
   const [likedCourses, setLikedCourses] = useState([]);
 
   function getCourses() {
-    let allCourses = [];
-    Object.values(courses).forEach((array) => {
-      array.forEach((courseData) => {
-        allCourses.push(courseData);
+    if (category === "All") {
+      let allCourses = [];
+      Object.values(courses).forEach((array) => {
+        array.forEach((courseData) => {
+          allCourses.push(courseData);
+        });
       });
-    });
-    return allCourses;
+      return allCourses;
+    } else {
+      //main sirf specific categiry ka data array krunga
+      return courses[category];
+    }
   }
+
   return (
-    <div>
+    <div className="flex flex-wrap justify-center gap-4 mb-4">
       {getCourses().map((course) => (
-        <Card key={course.id} course={course} />
+        <Card
+          key={course.id}
+          course={course}
+          likedCourses={likedCourses}
+          setLikedCourses={setLikedCourses}
+        />
       ))}
     </div>
   );
